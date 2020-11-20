48e11e33a257b23b4e487e1cd44214c9c9904664
Jesse <moore.jesse@gmail.com>
2020-11-20 09:10:39 -0600

    Added graphQL and express to cloud function

--- server-cloud-function/functions/src/index.ts
+++ server-cloud-function/functions/src/index.ts
@@ -1,10 +1,35 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+import schema from './graphql/schema';
+import resolvers from './graphql/resolvers';
+
 admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((_request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Firebase!');
-});
+function gqlServer() {
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        typeDefs: schema,
+        resolvers,
+        // Enable graphiql gui
+        introspection: true,
+        playground: true,
+    });
+
+    apolloServer.applyMiddleware({ app, path: '/api', cors: true });
+
+    app.get('/', (_req, res) => {
+        res.send('Hello from Firebase!');
+    });
+
+    return app;
+}
+
+const server = gqlServer();
+
+export const helloWorld = functions.https.onRequest(server);
