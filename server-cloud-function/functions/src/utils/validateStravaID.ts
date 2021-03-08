1ef6370f322a6d535bcb5638b57979c095c48c26
Jesse <moore.jesse@gmail.com>
2021-03-08 08:37:57 -0600

    made sure id is a number

--- server-cloud-function/functions/src/utils/validateStravaID.ts
+++ server-cloud-function/functions/src/utils/validateStravaID.ts
@@ -1,5 +1,5 @@
-export const validateStravaID = (id: String): Boolean => {
+export const validateStravaID = (id: string): Boolean => {
     if (id.length === 0) return false;
     if (id.length > 20) return false;
-    return true;
+    return !Number.isNaN(Number.parseInt(id));
 };
