2d050fc367b5a688e1ebe21ba724495a08532aa9
Jesse <moore.jesse@gmail.com>
2021-03-30 16:48:55 -0500

    added best efforts to activities

--- functions/src/helpers/getNewActivity.ts
+++ functions/src/helpers/getNewActivity.ts
@@ -9,6 +9,23 @@ export default async (
     const newActivity = await fetchActivityFromStrava(accessToken, id);
     if (newActivity === null) return null;
     newActivity.strava_id = newActivity.id;
+    newActivity.best_efforts = parseBestEfforts(newActivity);
     const activity = new Activity(newActivity);
     return activity;
 };
+
+function parseBestEfforts({ best_efforts }: any): BestEfforts[] | null {
+    if (!best_efforts || !Array.isArray(best_efforts)) return null;
+    return best_efforts.map((effort) => {
+        const { name, start_index, end_index, elapsed_time, distance } = effort;
+        return { name, start_index, end_index, elapsed_time, distance };
+    });
+}
+
+interface BestEfforts {
+    name: string;
+    start_index: number;
+    end_index: number;
+    elapsed_time: number;
+    distance: number;
+}
